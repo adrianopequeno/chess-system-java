df60f91da1013dd8be93db15bd44da455b685f4a
Adriano Pequeno <adrian.pekeno@gmail.com>
2021-09-30 10:43:03 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -28,5 +28,13 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	public Piece piece(Integer row, Integer column) {
+		return pieces[row][column];
+	}
+	
+	// sobrecarga do métod piece
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 	
 }
