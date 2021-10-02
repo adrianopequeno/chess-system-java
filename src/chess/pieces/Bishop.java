118987ba3420bc191f9c106c4f16340c324f41c2
Adriano Pequeno <adrian.pekeno@gmail.com>
2021-10-02 18:36:32 -0300

    Bishop

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,67 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "B";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getColumns()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+		// verifica se existe casa vazia acima da peça
+		// nw
+		p.setValues(position.getRow() - 1, position.getColumn() -1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiace(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() -1, p.getColumn() -1);
+		}
+		// verifica se existe peça em uma casa
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p))
+			mat[p.getRow()][p.getColumn()] = true;
+
+		// no
+		p.setValues(position.getRow() -1 , position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiace(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);;
+		}
+		// verifica se existe peça em uma casa
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p))
+			mat[p.getRow()][p.getColumn()] = true;
+
+		// se
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiace(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
+		}
+		// verifica se existe peça em uma casa
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p))
+			mat[p.getRow()][p.getColumn()] = true;
+
+		// sw
+		p.setValues(position.getRow() + 1, position.getColumn() -1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiace(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() -1);
+		}
+		// verifica se existe peça em uma casa
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p))
+			mat[p.getRow()][p.getColumn()] = true;
+
+		return mat;
+	}
+}
